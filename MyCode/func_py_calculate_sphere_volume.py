427ef89307befd9ed7155e91aeadf16bc19bec2e
bob8848 <bob8848@yeah.net>
2024-08-15 22:10:53 +0900

    Create func_py_calculate_sphere_volume.py

---
+++ MyCode/func_py_calculate_sphere_volume.py
@@ -0,0 +1,7 @@
+# func_py_calculate_sphere_volume.py
+import math
+
+def func_py_calculate_sphere_volume(radius):
+    return (4 / 3) * math.pi * radius**3
+
+print(func_py_calculate_sphere_volume(3))
