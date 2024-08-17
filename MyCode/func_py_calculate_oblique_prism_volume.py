36342ccde4a98e34a2be88fb5739f4c83c0d5a53
bob8848 <bob8848@yeah.net>
2024-08-17 20:19:54 +0900

    Create func_py_calculate_oblique_prism_volume.py

---
+++ MyCode/func_py_calculate_oblique_prism_volume.py
@@ -0,0 +1,5 @@
+# func_py_calculate_oblique_prism_volume.py
+def func_py_calculate_oblique_prism_volume(base_area, height):
+    return base_area * height
+
+print(func_py_calculate_oblique_prism_volume(20, 10))
